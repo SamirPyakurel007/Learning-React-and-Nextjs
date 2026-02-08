55f7eccf0c88865c5a5bec5947c0f1685303f6d2
samir <samirpyakurel07@gmail.com>
2026-02-08 12:13:30 +0545

    init quiz app

--- 25practice-useReducer/src/App.js
+++ 25practice-useReducer/src/App.js
@@ -1,9 +1,39 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  //loading, ready, error, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("action unknown");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { status, questions } = state;
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main></Main>
     </div>
   );
 }
