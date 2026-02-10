37285bb3578b5d0d3ecb71edf3522e955234e2bb
samir <samirpyakurel07@gmail.com>
2026-02-10 15:14:52 +0545

    feat: page layout setup

---
+++ 26reactrouter-contextapi--practice/src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <AppNav />
+      <p>APP</p>
+    </div>
+  );
+};
+
+export default AppLayout;
