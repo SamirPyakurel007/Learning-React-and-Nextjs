2e58c3e51db277aca3ae9acee5dd1d57abd591a0
samir <samirpyakurel07@gmail.com>
2026-02-11 14:22:24 +0545

    feat: using hooks in app

--- 28redux-toolkit-query/src/App.jsx
+++ 28redux-toolkit-query/src/App.jsx
@@ -1,5 +1,20 @@
+import { useCreatePostMutation, useGetPostsQuery } from "./state/api";
+
 function App() {
-  return <h1>hello redux</h1>;
+  const {
+    data: posts,
+    error: getPostError,
+    isLoading: isPostLoading,
+  } = useGetPostsQuery();
+
+  const [createPost] = useCreatePostMutation();
+
+  if (isPostLoading) return <p>Loading...</p>;
+
+  if (getPostError) return <p>There is an error...</p>;
+
+  console.log(posts);
+  return <h1>there are 100 resources</h1>;
 }
 
 export default App;
--- 28redux-toolkit-query/src/state/redux.js
+++ 28redux-toolkit-query/src/state/redux.js
@@ -1,16 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "./api";
-import globalReducer from "/state";
 
 const store = configureStore({
   reducer: {
-    global: globalReducer,
     [api.reducerPath]: api.reducer,
   },
-  middleware: (getDefaultMiddleware) => {
-    getDefaultMiddleware().concat(api.middleware);
-  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 setupListeners(store.dispatch);
